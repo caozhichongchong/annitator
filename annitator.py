2b61cae9ede387b5327af8cfdd113eaf89a83477
Jeffry Gaston <gastoj3@gmail.com>
2020-06-20 17:56:09 +0100

    parser cleanup

--- annitator.py
+++ annitator.py
@@ -2,17 +2,6 @@
 
 import argparse,sys
 
-def usage():
-    print("""
-
-    Usage: annotator.py --input input.csv [--output output.csv]
-
-    Annotates genes using the fastANNI algorithm
-    
-    Takes as input csv file of genes and adds more columns containing annotation information
-    """)
-
-    sys.exit(1)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -27,6 +16,7 @@ def main():
     args = parser.parse_args()
     print("Input = " + str(args.input))
     print("Output = " + str(args.output))
+
 
 if __name__ == "__main__":
     main()
